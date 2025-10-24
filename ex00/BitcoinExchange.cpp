1d2e09b930be179351cca3a42021bbfb02c23344
tndreka <tndreka@student.42heilbronn.de>
2025-10-24 03:48:38 +0200

    ADD: started to parse the argument file. check if the name is correct if not display error. also removed the healper functions from here to a utilities file

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -20,9 +20,16 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other)
 BitcoinExchange::~BitcoinExchange()
 {}
 
-// void BitcoinExchange::parseInputFile(const std::string& filename)
-// {
-// }
+bool BitcoinExchange::parseInputFile(const std::string& filename)
+{
+    std::ifstream file(filename.c_str());
+    if (!file.is_open())
+    {
+        std::cerr << "Error: could not open the file." << std::endl;
+        return false;
+    }
+    return true;
+}
 
 bool BitcoinExchange::load_data(const std::string& database)
 {
@@ -70,81 +77,8 @@ bool BitcoinExchange::load_data(const std::string& database)
     return true;
 }
 
-bool BitcoinExchange::isValidDate(const std::string& date)
-{
-    //bad format
-    if(date.length() != 10 || date[4] != '-' || date[7] != '-')
-        return false;
-    _year = date.substr(0, 4);
-    _month = date.substr(5, 2);
-    _day = date.substr(8, 2);
 
-    for (size_t i = 0; i < _year.length(); i++)
-    {
-        if (!isdigit(_year[i]))
-        {
-            std::cout << "year error\n";
-            return false;
-        }
-    }
-    for (size_t i = 0; i < _month.length(); i++)
-    {
-        if (!isdigit(_month[i]))
-        {
-            std::cout << "month error\n";
-            return false;
-        }
-    }
-    for (size_t i = 0; i < _day.length(); i++)
-    {
-        if (!isdigit(_day[i]))
-        {
-            std::cout << "day error\n";
-            return false;
-        }
-    }
-    int month = atoi(_month.c_str());
-    int day = atoi(_day.c_str());
-    if (month < 1 || month > 12)
-        return false;
-    if (day < 1 || day > 31)
-        return false;
-    return true;
-}
 
-std::string BitcoinExchange::trim(const std::string& line)
-{
-    size_t start = 0;
-    size_t end = line.length();
-    while (start < end && isspace(static_cast<unsigned char>(line[start])))
-        start++;
-    while (end > start && isspace(static_cast<unsigned char>(line[end - 1])))
-        end--;
-    return line.substr(start, end - start);
-}
-
-bool BitcoinExchange::isValidRate(const std::string& rate)
-{
-    bool dot = false;
-    if (rate.empty())
-        return false;
-    for (size_t i = 0; i < rate.length(); i++)
-    {
-        if (rate[i] == '.')
-        {
-            if (dot)
-                return false;
-            dot = true;
-        }
-        else if (!isdigit(rate[i]))
-            return false;
-    }
-    float value = atof(rate.c_str());
-    if (value < 0.0f)
-        return false;
-    _rate = value;
-    return true;
-}
 
 // void BitcoinExchange::printDatabase() const
 // {
