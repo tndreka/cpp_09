adfefc8d9447049e463ecad979ecdc3b1ab08f16
Taulant Ndreka <tndreka@2-D-9.42heilbronn.de>
2025-12-02 14:59:18 +0100

    ADD: main loop of the program

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,11 +6,23 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 22:18:14 by tndreka           #+#    #+#             */
-/*   Updated: 2025/11/04 22:18:26 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/12/02 14:58:42 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+int main(int ac, char *av[])
 {
+    if(ac != 2)
+    {
+        std::cerr << "Error\n";
+        return 1;
+    }
+    try
+    {}
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }   
     return 0;
 }
