2bf9f55719c18c466ed8ed347197e60465437ca3
Taulant Ndreka <tndreka@3-H-4.42heilbronn.de>
2025-12-03 15:12:07 +0100

    ADD: created the header file and cpp file and initialized the canonical form

---
+++ ex02/PmergeMe.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 15:03:06 by tndreka           #+#    #+#             */
+/*   Updated: 2025/12/03 15:09:39 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe(){}
+
+PmergeMe::PmergeMe(const PmergeMe& other){}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other){}
+
+PmergeMe::~PmergeMe(){}
---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 15:02:52 by tndreka           #+#    #+#             */
+/*   Updated: 2025/12/03 15:10:57 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMEREGEME_HPP
+#define PMEREGEME_HPP
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <sys/time.h>
+#include <algorithm>
+#include <stdexcept>
+#include <cstdlib>
+#include <sstream>
+
+class PmergeMe
+{
+private:
+    std::vector<int> _vector;
+    std::deque<int> _deque;
+    double _timer;
+        
+public:
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
+};
+
+
+#endif
