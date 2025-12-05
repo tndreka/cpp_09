3c4409c60a45cfd11b781e9eaa4f712ec8c00209
Taulant Ndreka <tndreka@2-E-9.42heilbronn.de>
2025-12-05 14:09:59 +0100

    ADD: created a parser function so the code looks more clean

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,11 +6,12 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 22:18:14 by tndreka           #+#    #+#             */
-/*   Updated: 2025/12/03 16:37:51 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/12/05 14:08:58 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+
 
 int main(int ac, char* av[])
 {
@@ -21,27 +22,12 @@ int main(int ac, char* av[])
     }
     else
     {
-        std::cout << "Start Sorting\n";
         if(PmergeMe::isValidInput(av) != 0)
         {
             std::cerr << "Error : Usage ./PmergeMe <positive integer sequence\n>";
             return 1;
         }
-        std::vector<int> input;
-        for (int i = 1; i < ac; ++i)
-        {
-            input.push_back(std::atoi(av[i]));
-        }
-        std::cout << "Before: ";
-        PmergeMe::print_stack_(input);
-        PmergeMe lets_sort;
-        //here sort on vector
-         // --> mergesort algo
-        lets_sort.sortVector(input);
-        //here sort on deque
-        const std::vector<int>& sorted_vec = lets_sort.getVector();
-        std::cout << "After: ";
-        PmergeMe::print_stack_(sorted_vec);
+        PmergeMe::parse_and_sort(ac, av);  
     }
     return 0;
 }
